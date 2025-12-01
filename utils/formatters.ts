5ace36fa518353f163776218a8a653a22c641812
dunnekim <doohyunkim@outlook.com>
2025-12-02 01:24:01 +0900

    Delete utils directory

--- utils/formatters.ts
+++
@@ -1,9 +0,0 @@
-
-export function formatNumber(value: number, decimals: number = 0): string {
-  // Use "ko-KR" for standard 3-digit comma separation.
-  // Default to 0 decimals for KRW (integers), but allow override.
-  return value.toLocaleString("ko-KR", {
-    minimumFractionDigits: 0,
-    maximumFractionDigits: decimals,
-  });
-}
